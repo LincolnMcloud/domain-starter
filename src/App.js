11a820f36c00ed1596925f86e2d24abc9e332890
LincolnMcloud <lamcloudjr@gmail.com>
2022-03-14 01:41:31 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -46,21 +46,18 @@ const App = () => {
 	}
 
 	// Gotta make sure this is async.
-	const checkIfWalletIsConnected = async() => {
-		// First make sure we have access to window.ethereum
+	const checkIfWalletIsConnected = async () => {
 		const { ethereum } = window;
 
 		if (!ethereum) {
-			console.log("Make sure you have MetaMask!");
+			console.log('Make sure you have metamask!');
 			return;
 		} else {
-			console.log("We have the ethereum object", ethereum);
+			console.log('We have the ethereum object', ethereum);
 		}
-
-		// Check if we're authorized to access the user's wallet
+		
 		const accounts = await ethereum.request({ method: 'eth_accounts' });
 
-		// Users can have multiple authorized accounts, we grab the first one if its there!
 		if (accounts.length !== 0) {
 			const account = accounts[0];
 			console.log('Found an authorized account:', account);
@@ -68,7 +65,18 @@ const App = () => {
 		} else {
 			console.log('No authorized account found');
 		}
-	}
+		
+		// This is the new part, we check the user's network chain ID
+		const chainId = await ethereum.request({ method: 'eth_chainId' });
+		setNetwork(networks[chainId]);
+
+		ethereum.on('chainChanged', handleChainChanged);
+		
+		// Reload the page when they change networks
+		function handleChainChanged(_chainId) {
+			window.location.reload();
+		}
+	};
 	
 
 	// Create a function to render if wallet is not connected yet
@@ -83,6 +91,15 @@ const App = () => {
 	
 	// Form to enter domain name and data
 	const renderInputForm = () =>{
+		if (network !== 'Polygon Mumbai Testnet') {
+			return (
+				<div className="connect-wallet-container">
+					<h2>Please switch to Polygon Mumbai Testnet</h2>
+					{/* This button will call our switch network function */}
+					<button className='cta-button mint-button' onClick={switchNetwork}>Click here to switch</button>
+				</div>
+			);
+		}
 		return (
 			<div className="form-container">
 				<div className="first-row">
@@ -165,6 +182,47 @@ const App = () => {
 	  catch(error){
 		console.log(error);
 	  }
+	}
+
+	const switchNetwork = async () => {
+		if (window.ethereum) {
+			try {
+				// Try to switch to the Mumbai testnet
+				await window.ethereum.request({
+					method: 'wallet_switchEthereumChain',
+					params: [{ chainId: '0x13881' }], // Check networks.js for hexadecimal network ids
+				});
+			} catch (error) {
+				// This error code means that the chain we want has not been added to MetaMask
+				// In this case we ask the user to add it to their MetaMask
+				if (error.code === 4902) {
+					try {
+						await window.ethereum.request({
+							method: 'wallet_addEthereumChain',
+							params: [
+								{	
+									chainId: '0x13881',
+									chainName: 'Polygon Mumbai Testnet',
+									rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
+									nativeCurrency: {
+											name: "Mumbai Matic",
+											symbol: "MATIC",
+											decimals: 18
+									},
+									blockExplorerUrls: ["https://mumbai.polygonscan.com/"]
+								},
+							],
+						});
+					} catch (error) {
+						console.log(error);
+					}
+				}
+				console.log(error);
+			}
+		} else {
+			// If window.ethereum is not found then MetaMask is not installed
+			alert('MetaMask is not installed. Please install it to use this app: https://metamask.io/download.html');
+		} 
 	}
 
 	// This runs our function when the page loads.
